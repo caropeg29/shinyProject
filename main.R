0c6ce50bf2545196775c4ff0a1d2949598804264
Caroline <caropeg29@gmail.com>
2018-11-13 11:06:51 +0100

    main

--- main.R
+++ main.R
@@ -91,27 +91,34 @@ ui <- fluidPage(
                fluidRow(column(6,plotOutput("diagDevices")),
                         column(6,textOutput("texteDiagDevices")))
                         ),
-               #tabPanel("Pays",
-                #        ),
                tabPanel("Chargement",
-                        fluidRow(DT::dataTableOutput(outputId = "tableEffectifsNbPage")),
                         fluidRow(DT::dataTableOutput(outputId = "tableEffectifsTemps")),
                         fluidRow(
-                          column(6,plotOutput(outputId = "effectifDiagNbPage")),
-                          column(6,plotOutput(outputId = "effectifDiagTemps"))
+                          column(6,plotOutput(outputId = "effectifDiagTemps")),
+                          column(6,plotOutput(outputId = "effectifCumuleTemps"))
                         ),
                         fluidRow(
-                          column(6,plotOutput(outputId = "effectifCumuleNbPage")),
-                          column(6,plotOutput(outputId = "effectifCumuleTemps"))
-                        ), 
-                        fluidRow(
-                          column(6,plotOutput(outputId = "boiteMoustacheNbPage")),
                           column(6,plotOutput(outputId = "boiteMoustacheTemps"))
                         ),
                         fluidRow(
                           textOutput("texteAnalyseTempsPage")
                         )
                         ),
+               tabPanel("Pages visitées",
+                 fluidRow(DT::dataTableOutput(outputId = "tableEffectifsNbPage")),
+                 fluidRow(
+                   column(6,plotOutput(outputId = "effectifDiagNbPage")),
+                   column(6,plotOutput(outputId = "effectifCumuleNbPage"))
+                   ),
+                 fluidRow(
+                   column(6,plotOutput(outputId = "boiteMoustacheNbPage"))
+                 ),
+                 fluidRow(
+                   textOutput("textePage")
+                 )
+               ),
+               #tabPanel("Page visitée",
+               #         ),
                tabPanel("Performances",
                         fluidRow(plotOutput("diagPerf")),
                         fluidRow(column(4,
@@ -129,28 +136,37 @@ ui <- fluidPage(
              
              
     tabPanel("Analyses Bivariées", 
-             
-             fluidRow(
-               column(6,
-                      plotOutput("nuagePointsTempsPage"),
-                      textOutput("correlationTempsPage")
-               ),
-               column(6,
-                      plotOutput("nuagePointsTempsChargement"),
-                      textOutput("correlationTempsChargement")
-               )
-             ),
-             fluidRow(
-               column(12, 
-                      plotOutput("barplotDodgeBiGenreSession"))
-               
-             ),
-             fluidRow(
-               column(12,
-                      plotOutput("barplotDodgeBiDeviceChargement"))
+             tabsetPanel(
+               tabPanel("Le temps utilisateurs",
+                        fluidRow(
+                          plotOutput("nuagePointsTempsPage"),
+                          textOutput("correlationTempsPage")
+                        ),
+                        fluidRow(
+                          plotOutput("nuagePointsTempsChargement"),
+                          textOutput("correlationTempsChargement")
+                        )
+                    ),
+               tabPanel("L'utilisateur",
+                        fluidRow(
+                           plotOutput("barplotDodgeBiGenreSession"),
+                           textOutput("texteBarplotSession")
+                        ),
+                        fluidRow(
+                          plotOutput("barplotDodgeBiDeviceChargement"),
+                          textOutput("texteBarplotDevice")
+                        )
+                    )
              )
              
-    )
+             
+             
+             
+    ),
+    
+    tabPanel("Conclusion",
+             textOutput("conclusion")
+             )
              
              
              )
@@ -198,32 +214,50 @@ server <- function(input,output){
     "I. Choix du Jeu de données"
   )
   output$textePremierePartie <- renderText(
-    "J'ai choisi des données concernants un site e-commerce. En effet, mon mémoire de fin d'année portant sur le e-commerce avec notamment la problématique suivante : Comment les choix stratégiques d’un pôle d’analyse
-de comportement utilisateur peuvent influencer le Système d’information d’un site e-commerce et
-    quels sont leurs enjeux ? 
+    "
+    Nous avons choisi des données concernant un site e-commerce dans la vente de bijoux et accessoires. On cherchera à prouver que l’étude des statistiques d’un site est un enjeu majeur pour la prise de décisions stratégique pour l’entreprise.
+
+    On émettra ainsi la problématique suivante :
     
-    Afin de répondre à cette problématique, il est très intéressant d'analyser le comportement de l'utilisateur sur le site, c'est pourquoi mes données concernent les actions de l'utilisateur sur le site.
+    Comment les choix stratégiques d’un pôle d’analyse de comportement utilisateur d’un site e-commerce, révèle les dysfonctionnements du système d’information et détermine les choix stratégiques de l’orientation de ce dernier ?
     
-    On peut trouver les variables suivantes :
-    - 'id' : l'identifiant de l'utilisateur est une variable quantitative continue permettant d'identifier l'utilisateur.
-    - 'sexe' : le sexe de l'utilisateur est une variable qualitative nominale permettant d'identifier le sexe de l'utilisateur. Cela nous permettra d'orienter nos choix stratégiques en fonction de la clientèle.
-    - 'device' : la device de l'utilisateur est une variable qualitative nominale permettant d'identifier par quel appareil l'utilisateur se connecte au site. Cela nous permettra d'orienter nos choix pour améliorer les devices les plus utilisés.
+    Afin de répondre à cette problématique, nous allons analyser l’utilisation du site par un échantillon de 1000 d’utilisateurs. 
+    
+    On trouvera ainsi les variables suivantes :
+    
+    - 'id' : l'identifiant de l'utilisateur est une variable quantitative discrète permettant d'identifier l'utilisateur.
+    
+    - 'sexe' : le sexe de l'utilisateur est une variable qualitative nominale permettant d'identifier le sexe de l'utilisateur. Cela nous permettra d'orienter nos choix stratégiques en fonction de la clientèle. 
+    
+    - 'device' : le device de l'utilisateur est une variable qualitative nominale permettant d'identifier par quel appareil l'utilisateur se connecte au site. Cela nous permettra d'orienter nos choix pour améliorer les devices les plus utilisés.
+    
     - 'pays' : le pays est une variable qualitative nominale permettant d'identifier le pays dans lequel l'utilisateur se connecte.
-    - 'age' : l'age de l'utilisateur est une variable quantitative continue permettant d'identifier l'age de l'utilisateur.
-    - 'temps_site' : la variable temps_site est une variable quantitative continue permettant d'identifier le temps de session en seconde sur le site de l'utilisateur. Cela nous donne une indication sur le fait que le site est attractif ou non.
-    - 'nb_page' : la variable nb_page est une variable quantitative continue permettant d'identifier le nombre de page que l'utilisateur a visité sur le site. Cela nous donne une indication sur le fait que le site est attractif ou non.
-    - 'moyenne_temps_page' : la variable moyenne_temps_page est une variable quantitative continue permettant d'identifier le temps moyen passé sur une page en seconde.
-    - 'temps_chargement_page' : la variable temps_chargement_page est une variable quantitative continue permettant d'identifier le temps moyen de chargement d'une page en seconde. Cela peut nous permettre d'améliorer les performances du site si besoin."
-  )
+    
+    - 'age' : l'âge de l'utilisateur est une variable quantitative discrète permettant d'identifier l'âge de l'utilisateur.
+    
+    - 'temps_site' : la variable temps_site est une variable quantitative continue permettant d'identifier le temps de session en seconde de l’utilisateur sur les pages du site. Cela nous donne une indication sur le fait que le site est attractif/séduisant ou non.
+    
+    - 'nb_page' : la variable nb_page est une variable quantitative continue permettant d'identifier le nombre de page que l'utilisateur a visité sur le site. Cela nous donne une indication sur le fait que le site est attractif/séduisant ou non.
+    
+    - 'moyenne_chargement’ : la variable temps_chargement_page est une variable quantitative continue permettant d'identifier le temps moyen de chargement d'une page en seconde. Cela nous donne une indication sur les performances du site.
+    
+    "
+    )
   output$titreDeuxiemePartie <- renderText(
-    "II. Objectif de l'analyse"
+    "II. Déroulement de l'analyse"
   )
   output$texteDeuxiemePartie <- renderText(
-    "Dans un premier temps, nous allons réaliser des analyses univariées dans le but de cibler les utilisateurs du site.
-    Nous allons donc comparer le pourcentage d'Homme et de Femme venant sur le site, les tranches d'ages des utilisateurs, les devices utilisées, les pays dans lesquels viennent les utilisateurs.
+    "
+    L’analyse est découpée en deux sections :
+
+    -	Une succession d’analyses univariées pour calculer en autre le nombre de page par utilisateur, le temps moyen d’une session utilisateur etc…
+    -	Une succession d’analyses bivariées pour déterminer l’existence de corrélations pour prouver l’attractivité du site ou déceler les problèmes de performance. 
+    -	Une conclusion apportant une réponse à notre problématique
     
-    Puis dans un second temps, notre objectif est d'améliorer les performances du site. Pour cela, nous allons étudier les corrélations entre le temps passé sur le site et le nombre de page, le device de l'utilisateur et le temps de chargement du site et enfin si le fait d'être un homme ou une femme impact le temps de session."
-  )
+    
+    Pour cela, nous allons étudier les corrélations entre le temps passé sur le site et le nombre de page, le device de l'utilisateur et le temps de chargement du site et enfin si le fait d'être une femme ou un homme impact le temps de session.
+    
+    ")
   output$titreTroisiemePartie <- renderText(
     "III. Jeu de données"
   )
@@ -300,8 +334,13 @@ de comportement utilisateur peuvent influencer le Système d’information d’u
     text_pie(pourcentages,c("67,9%","32,1%"),cex=1.1) # Ces valeurs en % sont à remplacer manuellement
     })
   output$texteDiagSexe <- renderText(
-    "Nous pouvons voir grâce à ce diagramme que nous avons une majorité de femme qui viennent sur le site par rapport aux hommes. Cela nous permettra de prendre des décisions stratégiques comme par exemple, continuer à attirer un public féminin ou bien au contraire, faire des changements sur le site pour attirer plus d'hommes."
-  )
+    "
+    Nous pouvons voir grâce à ce diagramme que nous avons une majorité de femme qui viennent sur le site par rapport aux hommes.
+
+    Cela nous permettra de prendre des décisions stratégiques comme par exemple, continuer à attirer un public féminin ou bien au contraire, faire des changements sur le site pour attirer plus d'hommes.
+    
+    "
+      )
   
   output$diagDevices <- renderPlot({
     pie(c(length(which((data()[,3])== "Desktop")),length(which((data()[,3])== "Mobile")),length(which((data()[,3])== "Tablette"))), col=c("#AAFFAA","#FFEE44","#DDDDDD"), labels=c("Desktop","Mobile","Tablette"), main="Comparaison sexe",cex=1.5)
@@ -327,8 +366,14 @@ de comportement utilisateur peuvent influencer le Système d’information d’u
     
     })
   output$texteDiagDevices <- renderText(
-    "Nous pouvons voir grâce à ce schéma que plus de la moitié des devices sont des téléphones mobiles, suivit des PC et enfin les tablettes. Nous savons donc qu'il faut qu'on axe nos stratégies d'évolution sur le site en fonction les smartphones."
-  )
+    "
+    Nous pouvons voir grâce à ce schéma que plus de la moitié des équipements utilisés sont des terminaux mobiles, suivi de poste fixe et enfin de tablette tactile.
+
+    Nous savons donc qu'il faut axer nos stratégies d'évolution sur le site en fonction de l’utilisation des smartphones dans notre société.
+
+    "
+    
+     )
   
   
   output$tableMoyenne <- renderTable({
@@ -355,8 +400,18 @@ de comportement utilisateur peuvent influencer le Système d’information d’u
   
   
   output$texteeffectifDiagAge <- renderText(
-    "Nous remarquons ainsi que le principal public touché sont des utilisateurs agés de 15 à 45ans avec une moyenne d'âge de 35ans. Par conséquent, nous devons petre en mesure d'adapté le contenu du site pour cette tranche d'âge ciblé. Nous pouvons également faire le choix de changer de stratégie et ainsi adapté le site pour un tout autre pulic. Cette analyse nous permet donc de prendre des choix stratégiques pour attirer du traffic sur le site."
-    )
+    "
+    Nous remarquons ainsi que le principal public touché sont des utilisateurs âgés de 15 à 45 ans représentant 907 personne avec une moyenne d'âge de 35 ans. 
+
+    Cependant, ce sont les 22-23 ans qui sont le plus touché par le design du site avec plus de 75 utilisateurs sur cette tranche.
+    
+    Par conséquent, nous devons être en mesure d'adapté le contenu du site pour cette tranche d'âge ciblé.
+    
+    Nous pouvons également faire le choix de changer de stratégie et ainsi adapté le site pour un tout autre public.
+    
+    "
+    
+     )
   output$effectifDiagAge <- renderPlot({ 
     plot(table(data()[,5]), col ="green4", xlab ="Ã¢ge", ylab ="Effectifs", 
          main ="Distribution des effectifs pour l'Ã¢ge")
@@ -408,7 +463,22 @@ de comportement utilisateur peuvent influencer le Système d’information d’u
     # Affichage complÃ©mentaires en Y des diffÃ©rents Ã¢ges
     rug(data()[,7], side = 2)
   })
-  
+  output$textePage <- renderText(
+    "
+    On observe que :
+
+    -	50 % des utilisateurs enregistrés visite plus de 6 pages avec un maximum à 12.
+    
+    Il serait intéressant d’observer ces pages afin de comprendre pourquoi le taux de rétention est plus élevé. 
+    
+    Les premières hypothèses peuvent-être : 
+    
+    -	Ce sont des pages d’achat. Ainsi, l’utilisateur est obligé de passer par ces pages.
+    -	Ce sont des pages « catalogues », cela indiquerait que l’utilisateur apprécie l’action de « cataloguer » sur le site.
+    -	Ce sont des pages d’information (évènement, promotion etc…). Et alors la fonctionnalité primaire du site n’est pas la vente en e-commerce mais la communication externe.
+    
+    "
+  )
   
   #Partie Temps
   
@@ -433,7 +503,20 @@ de comportement utilisateur peuvent influencer le Système d’information d’u
     rug(data()[,6], side = 2)
   })
   output$texteAnalyseTempsPage <- renderText(
-    "e"
+    "
+    On observe que :
+
+    -	50 % des utilisateurs enregistrés visite plus de 6 pages avec un maximum à 12.
+    
+    Il serait intéressant d’observer ces pages afin de comprendre pourquoi le taux de rétention est plus élevé. 
+    
+    Les premières hypothèses peuvent-être : 
+    
+    -	Ce sont des pages d’achat. Ainsi, l’utilisateur est obligé de passer par ces pages.
+    -	Ce sont des pages « catalogues », cela indiquerait que l’utilisateur apprécie l’action de « cataloguer » sur le site.
+    -	Ce sont des pages d’information (évènement, promotion etc…). Et alors la fonctionnalité primaire du site n’est pas la vente en e-commerce mais la communication externe.
+    
+    "
   )
   
   #Performance
@@ -489,6 +572,16 @@ de comportement utilisateur peuvent influencer le Système d’information d’u
   output$correlationTempsPage <- renderText({
     coeff.tmp <- cov(data()[, "temps_site"], data()[, "nb_page"])/(sqrt(var(data()[, "temps_site"])*var(data()[, "nb_page"])))
     paste("Coefficient de corrélation linéaire =", round(coeff.tmp,digits = 2))
+    "
+    On observe à travers cette droite une corrélation positive (0,8) entre le temps de session et le nombre de page visité. 
+
+Cela peut être interprété en plusieurs points :
+
+-	Les pages ne sont pas assez attractives car leur taux de rétention est trop faible et ainsi l’utilisateur change de page plus souvent.
+-	Le catalogue produit est bien trop grand et aucun produit phare n’attire principalement les utilisateurs.
+-	Le site est très attractif et capturant, pour étayer cette hypothèse il faudrait comparer cette donnée avec le panier de l’utilisateur et la somme dépensée.
+
+    "
   })
   
   output$nuagePointsTempsChargement <- renderPlot({
@@ -508,15 +601,53 @@ de comportement utilisateur peuvent influencer le Système d’information d’u
   output$correlationTempsChargement <- renderText({
     coeff.tmp <- cov(data()[, "temps_site"], data()[, "moyenne_chargement"])/(sqrt(var(data()[, "temps_site"])*var(data()[, "moyenne_chargement"])))
     paste("Coefficient de corrélation linéaire =", round(coeff.tmp,digits = 2))
+    "
+    On détermine qu’il n’existe aucune corrélation significative (-0.2) entre le temps d’une session et le temps de chargement moyen entre chaque page.
+
+    On en déduit un problème de performances/d’architecture car l’utilisateur à toujours le même temps de chargement quel que soit son temps de session active. Ce problème s’exprime par ailleurs par le fait que ¼ du temps d’une session utilisateur est bloqué par du chargement.
+    
+    Or, dans l’idéal, il faudrait obtenir une corrélation négative et tendre à augmenter cette corrélation. 
+    
+    En effet, l’utilisateur ne devrait pas recharger les éléments du site a chaque page (comme le header, le logo, le footer etc…) ou des informations clients cotés back-office (identifiant du visiteur, panier etc…·). 
+    
+    On pourrait résoudre cela par la mise en place d’un cache utilisateur et la réduction d’appel serveur.
+    
+    "
   })
   
   output$barplotDodgeBiGenreSession <- renderPlot({
     # Diagramme de profils entre les variables 'Level' et 'Sex'
     ggplot(data(), aes(x = sexe, fill = temps_site)) + geom_bar(position = "dodge")
   })
+  output$texteBarplotSession <- renderText(
+    "
+    Par ce diagramme on observe que le temps d’une session est plus importante lorsque l’utilisateur est une femme.
+
+    Deux axes stratégiques peuvent ainsi se dégager :
+    
+    -	Privilégier les utilisateurs « Femme » car il y a plus de chance qu’elle achète un produit.
+    -	Privilégier les utilisateurs « Homme » afin d’accéder à une nouvelle clientèle et d’influencer la prise de décision d’achat.
+    
+    "
+  )
   
   output$barplotDodgeBiDeviceChargement <- renderPlot({
     ggplot(data(), aes(x = device, fill = moyenne_chargement)) + geom_bar(position = "dodge")
+  })
+  output$texteBarplotSession <- renderText(
+    "
+    On observe que le device ayant le plus de problèmes de performance est l’équipement mobiles/smartphones.
+
+    On détecte ainsi un réel problème de performance sur cet équipement. Il faudrait réorienter le travail de l’équipe IT sur cette plateforme.
+    
+    "
+  )
+  
+  output$conclusion <- renderUI({
+    str1 <- paste("Grâce à ces analyses ont en déduit deux points importants :")
+    str2 <- paste("-	Le site actuel possède un design féminin sur la tranche d’âge 20-30 ans. ")
+    HTML(paste(str1, str2, sep = '<br/>'))
+    
   })
   
 }
